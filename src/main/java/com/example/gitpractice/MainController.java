df215f8a448d0f343391419bebf266bfb14b3d0b
gimminjae <min356812@naver.com>
2022-08-24 17:07:28 +0900

    c1

--- src/main/java/com/example/gitpractice/MainController.java
+++ src/main/java/com/example/gitpractice/MainController.java
@@ -1,4 +1,9 @@
 package com.example.gitpractice;
 
+import org.springframework.stereotype.Controller;
+
 public class MainController {
+    public static void main(String[] args[]) {
+        System.out.println(1);
+    }
 }
