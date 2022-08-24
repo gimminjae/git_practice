cd1e930456f58b97d9bece0ca4de7793c31e310b
gimminjae <min356812@naver.com>
2022-08-24 17:08:04 +0900

    c3

--- src/main/java/com/example/gitpractice/MainController.java
+++ src/main/java/com/example/gitpractice/MainController.java
@@ -6,5 +6,6 @@ public class MainController {
     public static void main(String[] args[]) {
         System.out.println(1);
         System.out.println(2);
+        System.out.println(3);
     }
 }
